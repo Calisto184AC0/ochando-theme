d31f0ffcc34c22feb490fa08f9f25ab459a96e6b
ARTURO RAÚL DE JUAN ACHAHUANCO <ardejua@upv.edu.es>
2021-06-26 11:56:08 +0200

    fin header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,11 @@
 let menuIcon = document.getElementsByClassName('och-header-menu-icon')[0]
 let menu = document.getElementsByClassName('och-nav')[0]
 menuIcon.onclick = () => {
-    if (menu.style.transform === "" || menu.style.transform === 'translateX(100%)') 
+    if (menu.style.transform === "" || menu.style.transform === 'translateX(100%)') {
         menu.style.transform = 'translateX(0%)'
-    else
+    }
+    else {
         menu.style.transform = 'translateX(100%)'
+    }
+        
 }

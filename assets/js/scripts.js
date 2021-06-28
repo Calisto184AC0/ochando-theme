769c6e05e23820c4536ef1a963f20c60d5cc75cf
ARTURO RAÚL DE JUAN ACHAHUANCO <ardejua@upv.edu.es>
2021-06-28 08:57:46 +0200

    empezando listas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,3 +9,21 @@ menuIcon.onclick = () => {
     }
         
 }
+
+let carousel = document.getElementsByClassName('och-carousel')[0]
+var cont = 0
+if (carousel != null) {
+    
+    let listController = document.getElementsByClassName('och-list-controller')
+    listController[0].onclick = () => {
+        cont++
+
+        carousel.style.transform = 'translate(' + cont * 100 + '%)'
+    }
+
+    listController[1].onclick = () => {
+        cont--
+
+        carousel.style.transform = 'translate(' + cont * 100 + '%)'
+    }
+}
